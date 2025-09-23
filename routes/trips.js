e37d3afba66445f607766bdf388952df74ab955a
Cédric <cedricauneau@gmail.com>
2025-09-23 17:49:45 +0200

    get trip

--- routes/trips.js
+++ routes/trips.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const trips = require('../models/trip');
 
-
+// recherche de trajet
+router.get("/", (req, res) => {
+    res.json({ trips });
+});
 
 module.exports = router;
