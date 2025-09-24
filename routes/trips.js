a64a03df96b03b7d7058b56d090b73be47c91ebf
Nour <nouramrine@gmail.com>
2025-09-24 13:31:01 +0200

    modification route trips  pour scriptV2

--- routes/trips.js
+++ routes/trips.js
@@ -1,25 +1,49 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const trips = require('../models/trip');
+const trips = require("../models/trip");
 
-// recherche des trajets
-router.get("/", (req, res) => {
-    trips.find().then(data => {
-        res.json({ trips: data });
-    });
+// // recherche des trajets
+// router.get("/", (req, res) => {
+//     trips.find().then(data => {
+//         res.json({ trips: data });
+//     });
+// });
+
+// // recherche des trajets par villes
+// router.get("/:departure", (req, res) => {
+//     trips.find({ departure: { $regex: new RegExp(req.params.departure, "i") },
+//     }).then(city => {
+//         if (city) {
+//             res.json({ result: true, trips: city });
+//         } else {
+//             res.json({ result: false, error: "City not found" });
+//         }
+//     });
+// });
+
+// route pour scriptV2
+
+router.get("/", async (req, res) => {
+  try {
+    const { departure, arrival, date } = req.query;
+
+    const filter = {};
+    if (departure)
+      filter.departure = { $regex: new RegExp(`^${departure}$`, "i") };
+    if (arrival) filter.arrival = { $regex: new RegExp(`^${arrival}$`, "i") };
+    if (date) {
+      const start = new Date(date);
+      const end = new Date(date);
+      end.setDate(end.getDate() + 1);
+      filter.date = { $gte: start, $lt: end };
+    }
+
+    const data = await trips.find(filter);
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
 });
-
-// recherche des trajets par villes
-router.get("/:departure", (req, res) => {
-    trips.find({ departure: { $regex: new RegExp(req.params.departure, "i") },
-    }).then(city => {
-        if (city) {
-            res.json({ result: true, trips: city });
-        } else {
-            res.json({ result: false, error: "City not found" });
-        }
-    });
-});
-
 
 module.exports = router;
